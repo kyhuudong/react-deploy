4056d5ea2fe4da69d30fe0f9a6dba48cc168d3aa
Dong Ky <kyhuudong@gmail.com>
2020-07-05 17:15:50 +0700

    Updates

---
+++ precache-manifest.110c257f221a8ea419e626b85a574354.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d0a767102a445cff8f762fd106efdb",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "6bb0b486981f6d248ffa",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6a10a78df6c76ca3a627",
+    "url": "/react-deploy/static/js/2.b1b31c97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.b1b31c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bb0b486981f6d248ffa",
+    "url": "/react-deploy/static/js/main.24c80d14.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
